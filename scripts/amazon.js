7c7ea44eac261bca0c864a496a9ea8b65623e113
ww590666 <2697319010@qq.com>
2024-11-29 16:25:07 +0800

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,5 @@
 // 有一系列产品，故使用array
 // 每个产品有很多属性，故使用object
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  // js的计算规则，需要先转化为整数
-  priceCents: 1090
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4,
-    count: 127 
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56 
-  },
-  priceCents: 799
-}];
-
 let productsHTML = '';
 
 products.forEach((product) => {
