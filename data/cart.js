8866841e535c19b11cee6324388c3f2d6211592a
ww590666 <2697319010@qq.com>
2024-11-30 16:35:08 +0800

    Delete products fro cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,13 @@
-export const cart = [{
+export let cart = [{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2
+  quantity: 1
 },{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
+  quantity: 2
 }];
 
 // 将函数化为更小的函数增加代码可读性
+// 用于将某个产品添加到购物车的函数
 export function addToCart(productId) {
   let matchingItem;
 
@@ -26,4 +27,21 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+}
+
+// 用于从购物车删除产品的函数
+export function removeFromCart(productId) {
+  // 1.创建一个新数组
+  const newCart = [];
+
+  // 2.循环遍历购物车cart
+  cart.forEach((cartItem) => {
+    // 如果不等于要删除的id就加入新数组
+    if(cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  // 更新购物车
+  cart = newCart;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -43,7 +43,7 @@ cart.forEach((cartItem) => {
             <span class="update-quantity-link link-primary">
               Update
             </span>
-            <span class="delete-quantity-link link-primary">
+            <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
@@ -99,5 +99,18 @@ cart.forEach((cartItem) => {
 
 });
 
+// 生成页面
 document.querySelector('.js-order-summary')
   .innerHTML = cartSummaryHTML;
+
+// 实现删除和更新的功能 添加监听器
+document.querySelectorAll('.js-delete-link')
+  .forEach((link) => {
+    link.addEventListener('click', () => {
+      // 实现步骤 1.从购物车里删了 2.更新HTML
+      // 获取数据属性以鉴别是哪个按钮被点了
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      console.log(cart);
+    })
+  });
