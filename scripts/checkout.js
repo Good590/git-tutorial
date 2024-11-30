b5dc3a979f1ff56de8a309a60ca45cb7755ab6ec
ww590666 <2697319010@qq.com>
2024-11-30 16:41:59 +0800

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,7 +20,7 @@ cart.forEach((cartItem) => {
   cartSummaryHTML += 
 
   `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -111,6 +111,10 @@ document.querySelectorAll('.js-delete-link')
       // 获取数据属性以鉴别是哪个按钮被点了
       const productId = link.dataset.productId;
       removeFromCart(productId);
-      console.log(cart);
+
+      const container = document.querySelector(
+        `.js-cart-item-container-${productId}`
+      );
+      container.remove();
     })
   });
